60a3dfe4e40f17491ecd1b97ed6352ba386ee3d1
synstarter <174186693+synstarter@users.noreply.github.com>
2025-01-14 13:11:52 +0100

    fixed img srcs

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ document.getElementById("signature-form").addEventListener("submit", function(ev
         <p style="margin: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="mailto:${email}">${email}</a></p>
         <p style="margin: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="tel:${phone}">${phone}</a></p>
         <p style="margin: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="https://pricespider.com" target="_blank">www.pricespider.com</a></p><br>
-        <img src="/imgs/logo.png" alt="Company Logo" width="236" height="auto" style="margin: 0px;">
+        <img src="imgs/logo.png" alt="Company Logo" width="236" height="auto" style="margin: 0px;">
         <p style= "margin: 0; font-size: 11pt; line-height: auto;">
-            <a href="https://www.facebook.com/Pricespider" target= "_blank"><img src="/imgs/fb.png" alt="Facebook Page" width="44" height="auto"></a>
-            <a href="https://twitter.com/pricespider" target= "_blank"><img src="/imgs/x.png" alt="Twitter Page" width="44" height="auto"></a>
-            <a href="https://www.linkedin.com/company/pricespider/" target= "_blank"><img src="/imgs/in.png" alt="LinkedIn Page" width="44" height="auto"></a>
+            <a href="https://www.facebook.com/Pricespider" target= "_blank"><img src="imgs/fb.png" alt="Facebook Page" width="44" height="auto"></a>
+            <a href="https://twitter.com/pricespider" target= "_blank"><img src="imgs/x.png" alt="Twitter Page" width="44" height="auto"></a>
+            <a href="https://www.linkedin.com/company/pricespider/" target= "_blank"><img src="imgs/in.png" alt="LinkedIn Page" width="44" height="auto"></a>
         </p>
         <p style= "margin: 10px 0; font-size: 10pt; color: #858585;"><i>This email is intended solely for the use of the individual(s) to whom it is addressed and may contain information that is privileged, confidential or otherwise required to be protected from disclosure. If the reader of this email is not the intended recipient, you are hereby notified that any dissemination, distribution, copying or other use of this communication or its attachments is strictly prohibited. If you have received this communication in error, please delete it as well as all copies or backups of it entirely from your email system and notify me that you received this email in error. Thank you.</i></p>
     </div>
