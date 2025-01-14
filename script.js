1457148f1614b0f97a74a31e16b5bcaf9317118c
synstarter <174186693+synstarter@users.noreply.github.com>
2025-01-14 18:15:58 +0100

    further generated signature formatting refinments

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ document.getElementById("signature-form").addEventListener("submit", function(ev
             <p style="margin: 0; padding: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="mailto:${email}">${email}</a></p>
             <p style="margin: 0; padding: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="tel:${phone}">${phone}</a></p>
             <p style="margin: 0; padding: 0; color: #0563c1; text-decoration: none; font-size: 11pt;"><a href="https://pricespider.com" target="_blank">www.pricespider.com</a></p><br>
-            <img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/logo.png" alt="Company Logo" width="236" height="auto" style="margin: 0; padding: 0;">
-            <p style= "margin: 0; padding: 0;">
-            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/fb.png" alt="Facebook Page" width="44" style="margin: 0; padding: 0;"></a>
-            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/x.png" alt="Twitter Page" width="44" style="margin: 0; padding: 0;"></a>
-            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/in.png" alt="LinkedIn Page" width="44" style="margin: 0; padding: 0;"></a>
+            <img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/logo.png" alt="Company Logo" style="margin: 0; padding: 0;">
+            <p style= "margin: 0; padding: 0; line-height: 1;">
+            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/fb.png" alt="Facebook Page" style="margin: 0; padding: 0;"></a>
+            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/x.png" alt="Twitter Page" style="margin: 0; padding: 0;"></a>
+            <a href="" target= "_blank"><img src="https://raw.githubusercontent.com/synstarter/ps-email-signature/20da807db0c46232f029e829e1d69bab0491107c/imgs/imgs2/in.png" alt="LinkedIn Page" style="margin: 0; padding: 0;"></a>
             </p>
             <p style= "margin: 10px 0; font-size: 10pt; color: #858585;"><i>This email is intended solely for the use of the individual(s) to whom it is addressed and may contain information that is privileged, confidential or otherwise required to be protected from disclosure. If the reader of this email is not the intended recipient, you are hereby notified that any dissemination, distribution, copying or other use of this communication or its attachments is strictly prohibited. If you have received this communication in error, please delete it as well as all copies or backups of it entirely from your email system and notify me that you received this email in error. Thank you.</i></p>
         </div>`;
